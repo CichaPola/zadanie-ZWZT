64f72c7b8c0142596e46ae33c868baa931764bdf
CichaPola <martakazanowska11@wp.pl>
2021-02-04 13:17:42 +0100

    add css

--- templates/home.js
+++ templates/home.js
@@ -4,13 +4,17 @@ const home = () => {
     script.setAttribute("type", "text/html");
     script.setAttribute("id", "home");
     script.innerHTML= 
-        `<form id="form">
-            <input id="username" type="text" placeholder="username"/><br>
-            <span id="errorMsg1"></span><td><br>
-            <input id="password" placeholder="password"/><br>
-            <span id="errorMsg2"></span></td><br>
-            <button class="submit">SUBMIT</button>
-        </form>`;
+        `
+        <div id="main">
+            <p id="signIn">SIGN IN</p>
+            <form id="form">
+                <input id="username" type="text" placeholder="username"/><br>
+                <span id="errorMsg1"></span><td><br>
+                <input id="password" placeholder="password"/><br>
+                <span id="errorMsg2"></span></td><br>
+                <button class="submit">SUBMIT</button>
+            </form>
+        <div>`;
     head.appendChild(script);
 };
 export default home();
