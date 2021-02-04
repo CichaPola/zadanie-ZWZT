dba2a7014dbb6ae784d567f137dfac24f288b65f
CichaPola <martakazanowska11@wp.pl>
2021-02-04 11:53:53 +0100

    add logging with axios

--- app.js
+++ app.js
@@ -1,7 +1,35 @@
 import { route } from './router';
 
 route('/', 'home', function() {
-  this.where = 'here';
+  this.$on('.submit', 'click', () => {
+
+    const username=document.getElementById('username').value;
+    const password=document.getElementById('password').value;
+    const data = {
+      username: username,
+      password: password, 
+    }
+    axios({
+      method: 'post',
+      url: 'https://zwzt-zadanie.netlify.app/api/login',
+      data: data
+    })
+    .then((response) => {
+      if(!username || !password) {
+        if(!username) {
+          document.getElementById('errorMsg1').innerText="fill in the field above";
+        }
+        if(!password) {
+          document.getElementById('errorMsg2').innerText="fill in the field above";
+        }
+      }
+      else {
+        window.location.href='#/ex1';
+      }
+    }, (error) => {
+      document.getElementById('errorMsg2').innerText="invalid password";
+    });
+  });
 });
 
 route('/ex1', 'example1', function() {
